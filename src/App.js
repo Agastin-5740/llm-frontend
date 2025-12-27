ddecea3eb34af0d9c5df2e19ca4db31247ae78f7
Agastin-5740 <snakehod@gmail.com>
2025-12-27 22:49:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
     setMessages(prev => [...prev, { type: "user", text: question }]);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/nl-query", {
+      const res = await fetch("https://llm-backend-g22t.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }),
